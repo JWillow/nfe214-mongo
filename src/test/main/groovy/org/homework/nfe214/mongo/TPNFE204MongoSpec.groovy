bd01293b29055f87ecc12ff15c1b46ab6a4c8fc5
JWillow <rletient@gmail.com>
2017-12-18 22:00:09 +0100

    Fin TP

--- src/test/main/groovy/org/homework/nfe214/mongo/TPNFE204MongoSpec.groovy
+++ src/test/main/groovy/org/homework/nfe214/mongo/TPNFE204MongoSpec.groovy
@@ -1,6 +1,7 @@
 package org.homework.nfe214.mongo
 
 import com.mongodb.client.DistinctIterable
+import com.mongodb.client.MapReduceIterable
 
 import static com.mongodb.client.model.Aggregates.*
 import static com.mongodb.client.model.Filters.*
@@ -163,7 +164,7 @@ class TPNFE204MongoSpec extends Specification {
     def "Compter le nombre de publications par auteur et trier le résultat par ordre croissant"() {
         when:
         def resultSet
-        resultSet = publisCollection.aggregate([ unwind('$authors'), group('$authors', sum("total", 1)), sort(ascending('total'))])
+        resultSet = publisCollection.aggregate([unwind('$authors'), group('$authors', sum("total", 1)), sort(ascending('total'))])
 
         then:
         resultSet.each {
@@ -171,5 +172,351 @@ class TPNFE204MongoSpec extends Specification {
         }
     }
 
+    def "Pour chaque document de type livre, émettre le document avec pour clé title"() {
+        setup:
+        String map = """
+            function() {
+                if(this.type == 'Book') {
+                    emit(this.title, this);
+                }
+            }
+        """
+        String reduce = """
+            function(title, docs) {
+               var result = new Object();
+               result.title = title;
+               result.docs = docs;
+               return result;
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
 
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "Pour chacun de ses livres, donner le nombre de ses auteurs"() {
+        setup:
+        String map = """
+            function() {
+                if(this.type == 'Book') {
+                    emit(this.title, this.authors.length);
+                }
+            }
+        """
+        String reduce = """
+            function(title, nbAuthors) {  
+                var result = new Object();
+                result.nbAuthors = nbAuthors;             
+                return result;
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "Pour chaque document ayant 'booktitle' (chapitre) publié par Springer, donner le nombre de ses chapitres"() {
+        setup:
+        String map = """
+            function() {
+              if(this.publisher == 'Springer' && this.booktitle) {
+                  emit(this.booktitle, 1);
+              }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.sum(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "Pour chaque éditeur « Springer », donner le nombre de publication par année"() {
+        setup:
+        String map = """
+            function() {
+              if(this.publisher == 'Springer') {
+                  emit(this.year, 1);
+              }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.sum(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "Pour chaque clé « publisher & année » (pour ceux qui ont un publisher), donner le nombre de publications"() {
+        setup:
+        String map = """
+            function() {
+               if(this.publisher) {
+                var document = new Object();
+                document.publisher = this.publisher
+                document.year = this.year              
+                emit(document, 1);
+               }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.sum(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "6 - Pour l’auteur « Toru Ishida », donner le nombre de publication par année"() {
+        setup:
+        String map = """
+            function() {
+                if(Array.contains(this.authors, "Toru Ishida")) {
+                    emit(this.year, 1);
+                }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.sum(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "7 - Pour l’auteur « Toru Ishida », donner le nombre moyen de pages pour ses articles (type Article)"() {
+        setup:
+        String map = """
+            function() {
+                if(this.type != 'Article') {
+                    return;
+                }
+                if(!Array.contains(this.authors, "Toru Ishida")) {
+                    return;
+                }
+                emit("Article", this.pages.end - this.pages.start)
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.avg(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    //too much data for in memory map/reduce
+    def "8 - Pour chaque auteur donner le titre de ses publications"() {
+        setup:
+        String map = """
+            function() {
+                for(var idx = 0; idx < this.authors.length; idx ++) {
+                    emit(this.authors[idx], this.title);
+                }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return {"livres":values};
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    //too much data for in memory map/reduce
+    def "9 - Pour chaque auteur, donner le nombre de publications associé à chaque année"() {
+        setup:
+        String map = """
+            function() {
+                for(var idx = 0; idx < this.authors.length; idx ++) {
+                    
+                    emit({author:this.authors[idx], year:this.year}, 1);
+                }
+            }
+        """
+        String reduce = """
+            function(key, values) {  
+                return Array.sum(values);
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "10 - Pour chaque auteur, donner le nombre de publications associé à chaque année"() {
+        setup:
+        String map = """
+            function() {
+                if(this.publisher != 'Springer') {
+                    return;
+                }
+            
+                for(var idx = 0; idx < this.authors.length; idx ++) {
+                    emit(this.year, this.authors);
+                }
+            }
+        """
+        String reduce = """
+            function(year, authorsWithDoublon) {  
+                var countedAuthors = []
+                for(var idx = 0; idx < authorsWithDoublon.length; idx ++) {
+                    if(Array.contains(countedAuthors, authorsWithDoublon[idx])) {
+                        continue
+                    }
+                    countedAuthors.push(authorsWithDoublon[idx]);
+                
+                }
+                return countedAuthors.length;
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "11 - Compter les publications de plus de 3 auteurs"() {
+        setup:
+        String map = """
+            function() {
+                if(this.authors.length > 3) {
+                    emit("COUNT",1);
+                }
+                
+            }
+        """
+        String reduce = """
+            function(count, values) {
+                return Array.sum(values);  
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "12 - Donner pour chaque publieur, donner le nombre moyen de pages par publication"() {
+        setup:
+        String map = """
+            function() {
+                if(!this.publisher || !this.pages || !this.pages.end || !this.pages.start) {
+                    return;
+                }
+                emit(this.publisher, this.pages.end - this.pages.start);
+            }
+        """
+        String reduce = """
+            function(publisher, pages) {
+                return Array.avg(pages);  
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
+
+    def "13 - Pour chaque auteur, donner le minimum et le maximum des années, ainsi que le nombre de publication totale"() {
+        setup:
+        String map = """
+            function() {            
+                for(var idx = 0; idx < this.authors.length; idx ++) {
+                    emit(this.authors[idx], {year:this.year});
+                }
+            }
+        """
+        String reduce = """
+            function(author, documents) {
+            
+                var minYear = 3000;
+                var maxYear = 0;
+                for(var i = 0; i < documents.length; i++) {
+                    minYear = Math.min(minYear, documents[i].year);
+                    maxYear = Math.max(maxYear, documents[i].year);
+                }  
+                return {min:minYear, max:maxYear, number:documents.length};
+            }    
+        """
+        when:
+        MapReduceIterable<?> result = publisCollection.mapReduce(map, reduce)
+
+        then:
+        result != null
+        result.each {
+            println it
+        }
+    }
 }
